255fb17c5c66596fb257575ed7567c2a70ff80c0
Esenou <superesenou.kg@gmail.com>
2018-11-28 18:16:53 +0600

    esen_1

---
+++ app/src/main/java/app/superesenou/ru/webrand/main/ui/descriptionmain/ImageActivity.kt
@@ -0,0 +1,12 @@
+package app.superesenou.ru.webrand.main.ui.descriptionmain
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image)
+    }
+}
